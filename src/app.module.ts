8cf27116ff5e26c1b01cc9dc417951aafd158a9d
CptTapo4ek <andreib@nextlogic.ro>
2024-02-19 13:56:55 +0200

    create/get tasks

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,27 @@
 import {Module} from "@nestjs/common";
 import {AppController} from "./app.controller";
 import {AppService} from "./app.service";
+import * as process from "process";
+import {ConfigModule} from "@nestjs/config";
+import {MongooseModule} from "@nestjs/mongoose";
+import { TaskModule } from './task/task.module';
 
+
+const environment = process.env.NODE_ENV || 'development';
 
 @Module({
     controllers: [AppController],
     providers: [AppService],
+    imports: [
+      TaskModule,
+      ConfigModule.forRoot({
+         envFilePath: `.env.${environment}`,
+         isGlobal: true,
+      }),
+      MongooseModule.forRoot(
+          process.env.MONGODB_WRITE_CONNECTION_STRING,
+      ),
+    ],
 })
-export  class AppModule {
 
-}
+export class AppModule {}
