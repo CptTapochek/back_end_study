d8ac122f7a3ef5f6fb3528c0ce78ec9c1226f8cc
CptTapo4ek <andreib@nextlogic.ro>
2024-02-20 01:22:48 +0200

    init user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,26 +1,22 @@
 import {Module} from "@nestjs/common";
-import {AppController} from "./app.controller";
-import {AppService} from "./app.service";
 import * as process from "process";
 import {ConfigModule} from "@nestjs/config";
 import {MongooseModule} from "@nestjs/mongoose";
-import { TaskModule } from './task/task.module';
+import { UsersModule } from './users/users.module';
 
 
 const environment = process.env.NODE_ENV || 'development';
 
 @Module({
-    controllers: [AppController],
-    providers: [AppService],
+    controllers: [],
+    providers: [],
     imports: [
-      TaskModule,
+      MongooseModule.forRoot(process.env.MONGODB_WRITE_CONNECTION_STRING),
       ConfigModule.forRoot({
          envFilePath: `.env.${environment}`,
          isGlobal: true,
       }),
-      MongooseModule.forRoot(
-          process.env.MONGODB_WRITE_CONNECTION_STRING,
-      ),
+      UsersModule,
     ],
 })
 
--- src/app.service.ts
+++
@@ -1,9 +0,0 @@
-import {Injectable} from "@nestjs/common";
-
-
-@Injectable()
-export class AppService {
-    getUsers() {
-        return [{id: 2, name: "test user 2"}]
-    }
-}
